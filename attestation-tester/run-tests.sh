2c24d0ed60217954c7dd37425f8f0bed968c2120
Paul-Georg Wagner <paul-georg.wagner@iosb.fraunhofer.de>
2023-07-19 14:19:02 +0000

    Added measurements on T14s

--- attestation-tester/run-tests.sh
+++ attestation-tester/run-tests.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo Running attestation protocol tests using the simulator...
-./build/install/attestation-tester/bin/attestation-tester --type ssl --type tap-uni --type tap --type tap-ssl --type tap-dh --type mscp --type mscp-ext --type mscp-org --n 100 > results-sim.log
+./build/install/attestation-tester/bin/attestation-tester --type ssl --type tap-uni --type tap --type tap-ssl --type tap-dh --type mscp --type mscp-ext --n 100 > results-sim.log
 echo Running attestation protocol tests using the physical device...
-./build/install/attestation-tester/bin/attestation-tester --device --type tap-uni --type tap --type tap-ssl --type tap-dh --type mscp --type mscp-ext --type mscp-org --n 100 > results-hw.log
+./build/install/attestation-tester/bin/attestation-tester --device --type tap-uni --type tap --type tap-ssl --type tap-dh --type mscp --type mscp-ext --n 100 > results-hw.log
